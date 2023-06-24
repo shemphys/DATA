947fca6e6b145fa45ee7d3f4438a99cd9a703a8c
Miguel Paredes <mykjestic@gmail.com>
2023-06-24 23:59:25 +0200

    Update first.py

--- Análisis de la Evolución de la Calidad del Aire en las Principales Ciudades del Mundo/first.py
+++ Análisis de la Evolución de la Calidad del Aire en las Principales Ciudades del Mundo/first.py
@@ -4,24 +4,34 @@ import pandas as pd
 # Endpoint de la API
 url = 'https://api.openaq.org/v2/measurements'
 
-# Parámetros de la consulta
-params = {
-    'city': 'London',
-    'parameter': 'pm25',
-    'date_from': '2013-01-01',
-    'date_to': '2023-01-01',
-    'limit': 10000,  # depende del número de resultados que desees obtener
-    'order_by': 'datetime'
-}
+cities = ['Tokyo', 'Delhi', 'Shanghai', 'São Paulo', 'Mumbai', 'Cairo', 'Mexico City', 'Beijing', 'Osaka', 'New York']
 
-# Realizar la petición a la API
-response = requests.get(url, params=params)
+# Query params
+for city in cities:
+    params = {
+        'city': city,
+        'parameter': 'pm25',
+        'date_from': '2013-01-01',
+        'date_to': '2023-01-01',
+        'limit': 10000,  # how many results you want?? 
+        'order_by': 'datetime'
+    }
 
-# Convertir la respuesta en un dataframe de pandas
-data = response.json()
-#print(data)
-df = pd.DataFrame(data['results'])
+    # API request
+    response = requests.get(url, params=params)
+
+    # Converting request into pandas dataframe
+    data = response.json()
+    #print(data)
+    df = pd.DataFrame(data['results'])
+
+    # Add current city to main dataframe
+    df_all = pd.concat([df_all, df])
 
 print(df.head())
-df.to_json('air_quality_data.json', orient='records', lines=True)
+df_all.to_json('air_quality_data.json', orient='records', lines=True)
 
+###############NOTES###############
+# hay que medir más niveles:
+# PM2.5, PM10, CO2, SO2, O3 y NO2
+# por ahora solo mido PM2.5
