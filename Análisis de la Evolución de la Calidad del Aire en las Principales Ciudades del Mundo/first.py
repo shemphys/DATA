cc0c8a8dd503c32cfcab3d526c0ade73e1cd5937
Miguel Paredes <mykjestic@gmail.com>
2023-06-25 14:50:33 +0200

    Update first.py

--- Análisis de la Evolución de la Calidad del Aire en las Principales Ciudades del Mundo/first.py
+++ Análisis de la Evolución de la Calidad del Aire en las Principales Ciudades del Mundo/first.py
@@ -1,32 +1,43 @@
 import requests
 import pandas as pd
+import time
 
 # Endpoint de la API
 url = 'https://api.openaq.org/v2/measurements'
 
 cities = ['Tokyo', 'Delhi', 'Shanghai', 'São Paulo', 'Mumbai', 'Cairo', 'Mexico City', 'Beijing', 'Osaka', 'New York']
 
+# List of pollutants to consider
+parameters = ['pm25', 'pm10', 'co2', 'so2', 'o3', 'no2']
+
+# Empty dataframe to storage data
+df_all = pd.DataFrame()
+
 # Query params
 for city in cities:
-    params = {
-        'city': city,
-        'parameter': 'pm25',
-        'date_from': '2013-01-01',
-        'date_to': '2023-01-01',
-        'limit': 10000,  # how many results you want?? 
-        'order_by': 'datetime'
-    }
+    for parameter in parameters:
+        params = {
+            'city': city,
+            'parameter': parameter,
+            'date_from': '2013-01-01',
+            'date_to': '2023-01-01',
+            'limit': 10000,  # how many results you want?? 
+            'order_by': 'datetime'
+        }
 
-    # API request
-    response = requests.get(url, params=params)
+        # API request
+        response = requests.get(url, params=params)
 
-    # Converting request into pandas dataframe
-    data = response.json()
-    #print(data)
-    df = pd.DataFrame(data['results'])
+        # Converting request into pandas dataframe
+        data = response.json()
+        #print(data)
+        df = pd.DataFrame(data['results'])
 
-    # Add current city to main dataframe
-    df_all = pd.concat([df_all, df])
+        # Add current city to main dataframe
+        df_all = pd.concat([df_all, df])
+
+        # 5s pause to avoid API requests per time limits
+        time.sleep(5)
 
 print(df.head())
 df_all.to_json('air_quality_data.json', orient='records', lines=True)
